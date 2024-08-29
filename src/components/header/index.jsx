f0ec60b6dcad404d92e699e7df161a5652d83685
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-08-29 16:52:14 -0300

    Alterações na parte de estilização e mudanças no html.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,50 +1,66 @@
 function Header() {
     return (
         <header className="w-full h-[156px] bg-[#E2F0E1]">
-            <div className="py-[28px] px-[188px] flex">
-                <img
-                    className="w-[95px] h-[98px]"
-                    src="./src/img/logoterrasaudade.svg" 
-                    alt="Logo do grupo escoteiro terra da saudade">
-                </img>
-                <p className="text-base py-[28px] px-[16px]">
-                    Grupo <span className="text-[#059A00] font-medium">Escoteiro</span> Terra Da <br></br>Saudade - 05/SP
-                </p>
-                {/* Links */}
-                <div className="absolute right-20 py-[28px] max-w-full">
-                    <div className="flex gap-[16px]">
-                        <a 
-                            href="#" 
-                            alt="Link da página Home"
-                        >
-                            <span className="text-[#059A00] border-b-2 border-green-600">Home</span>
-                        </a>
-                        <a 
-                            href="#"
-                            alt="Link da página Ramos"
-                        >
-                            Ramos
-                        </a>
-                        <a 
-                            href="#"
-                            alt="Link da página de Galeria"
-                        >
-                            Galeria
-                        </a>
-                        <a 
-                            href="#"
-                            alt="Link da página de Nossas Atividades"
-                        >
-                            Nossas atividades
-                        </a>
-                        <a 
-                            href="#"
-                            alt="Link da página de Login"
-                        >
-                            Login
-                        </a>
-                    </div>
+            <div className="px-[188px] py-[28px] flex justify-between items-center">
+                <div className="flex gap-[16px] items-center">
+                    <img
+                        className="w-[95px] h-[98px]"
+                        src="./src/img/logoterrasaudade.svg" 
+                        alt="Logo do grupo escoteiro terra da saudade"
+                    />
+                    <p className="text-base">
+                        Grupo <span className="text-[#059A00] font-medium">Escoteiro</span> Terra Da <br></br>Saudade - 05/SP
+                    </p>
                 </div>
+                <menu className="flex justify-end">
+                    <ul className="flex gap-[16px]">
+                        <li>
+                            <a 
+                                className="no-underline text-[#059A00] font-medium"
+                                href="#" 
+                                alt="Link da página Home"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="no-underline"
+                                href="#"
+                                alt="Link da página Ramos"
+                            >
+                                Ramos
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="no-underline"
+                                href="#"
+                                alt="Link da página de Galeria"
+                            >
+                                Galeria
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="no-underline"
+                                href="#"
+                                alt="Link da página de Nossas Atividades"
+                            >
+                                Nossas atividades
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="no-underline"
+                                href="#"
+                                alt="Link da página de Login"
+                            >
+                                Login
+                            </a>
+                        </li>
+                    </ul>
+                </menu>
             </div>
         </header>
     );
