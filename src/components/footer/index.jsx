f5a309d9a43baabd4f0c4d0603c9202993704f7d
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-08-31 20:27:17 -0300

    Alterações feitas, padronização ajustada.

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,10 @@
 import logo from '../../../public/logo.svg';
-import tailwindConfig from "../../../tailwind.config";
 
 function Footer() {
     return (
-        <footer className="w-full h-[234px] bg-light-social-brand absolute bottom-0">
+        <footer className="w-full h-[234px] bg-light-social-brand">
             <figure 
-                className="flex gap-12 items-center cursor-pointer px-[188px] pt-[70px]"
+                className="flex gap-12 items-center cursor-pointer px-48 pt-16"
                 title="Voltar à home"
                 aria-label="Voltar à página inicial do Grupo Escoteiro Terra Da Saudade"
             >
@@ -14,19 +13,17 @@ function Footer() {
                     src={logo}
                     alt="Árvore com um machado cravado no meio dela, diversos galhos e folhas."
                 />
-                <p
-                    className="max-w-[580px] text-[16px]"
-                >
+                <p className="max-w-[580px] text-base">
                     Juntos construímos um mundo melhor. Cada gesto transforma sonhos em realidade.
                     Lado a lado, desafios viram oportunidades e a esperança se torna ação. 
                     Somos parte de algo maior, e juntos, fazemos a diferença.
                 </p>
-                <caption className="text-[24px] pl-[80px] text-left">
+                <caption className="text-2xl pl-20 text-left">
                     Grupo <strong className="text-social-brand">Escoteiro</strong> Terra Da Saudade - 05/SP
                 </caption>
             </figure>
             <div className="w-full text-center pt-8">
-                <small className="text-social-gray">© Todos direitos reservados</small>
+                <small className="text-social-gray">&copy; Todos direitos reservados</small>
             </div>
         </footer>
     );
