f76658a3f17b75492b6eb9ff6b1f2ad0e728092d
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-09-01 19:29:07 -0300

    feature(component): add tabs component for settings page

---
+++ src/components/tabs/components/tab-header.jsx
@@ -0,0 +1,31 @@
+import { arrayOf, string, func, number } from "prop-types";
+
+function TabHeader({ titles, tab, changeTab }) {
+  return (
+    <div className="flex items-center border-b border-solid border-social-gray">
+      {titles.map((title, i) => {
+        return (
+          <p
+            key={i}
+            className={`
+              p-3 font-medium cursor-pointer hover:bg-social-gray
+              select-none transition-colors
+              ${tab === i ? 'bg-light-social-brand text-social-brand hover:bg-light-social-brand' : ''}
+            `}
+            onClick={() => changeTab(i)}
+          >
+            {title}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+TabHeader.propTypes = {
+  titles: arrayOf(string).isRequired,
+  tab: number.isRequired,
+  changeTab: func.isRequired,
+}
+
+export { TabHeader };
---
+++ src/components/tabs/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { arrayOf, string, node } from "prop-types";
+import { TabHeader } from "./components/tab-header";
+
+function Tabs({ body, titles }) {
+  const [tab, setTab] = useState(0)
+
+  function handleChangeTab(selectedTab) {
+    const alreadyIsActive = tab === selectedTab;
+    if(alreadyIsActive) return;
+
+    setTab(selectedTab);
+  }
+
+  return (
+    <div className="w-full">
+      <TabHeader
+        tab={tab}
+        titles={titles}
+        changeTab={handleChangeTab}
+      />
+      <div className="w-full pl-6 pt-8">
+        {body[tab]}
+      </div>
+    </div>
+  );
+}
+
+Tabs.propTypes = {
+  titles: arrayOf(string).isRequired,
+  body: arrayOf(node).isRequired,
+}
+
+export { Tabs };
