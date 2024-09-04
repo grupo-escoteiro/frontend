e99bb7c39a1bad46ee8b3ebd61594bb8a23c1fa2
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-09-04 20:30:29 -0300

    Alterações no card

--- src/components/cards/index.jsx
+++ src/components/cards/index.jsx
@@ -3,51 +3,51 @@ import { SocialIcon } from 'react-social-icons';
 
 function Cards() {
   return (
-    <div className="relative w-[22rem] h-[28rem]">
+    <div className="relative w-[22rem] h-[28rem] rounded overflow-hidden shadow-default">
       <div
-        className="relative flex flex-col items-center overflow-hidden w-[22rem] h-[28rem] 
+        className="relative flex flex-col items-center cursor-pointer overflow-hidden w-[22rem] h-[28rem] 
                    shadow-lg transition duration-500">
         <div
-          className="absolute inset-0 z-[2] transition duration-500 
-                     transform hover:-translate-y-[104px]">
+          className="absolute inset-0 z-10 transition duration-500 
+                     transform hover:-translate-y-28">
           <img 
-            className="w-full h-full object-cover" 
+            className="w-full h-full object-cover hover:brightness-50 transition duration-500 ease-in-out" 
             src={ryan} 
-            alt="Imagem de uma pessoa" 
+            alt="Pessoa sorrindo, cabelo preto e utilizando uma camisa preta" 
           />
-          <div
-            className="absolute inset-0 bg-black opacity-0 
-                       hover:opacity-60 transition duration-500 ease-in-out">
-          </div>
+          <ul className="absolute flex left-1/2 -translate-x-1/2 gap-8 bottom-6 z-10">
+            <li 
+              className="list-none" 
+              title="Instagram">
+              <a 
+                className="relative grid place-items-center rounded-lg transition duration-500 
+                         transform hover:-translate-y-2"
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer external nofollow"
+              >
+                <SocialIcon
+                  url="https://instagram.com"
+                  className="transition duration-500 transform hover:rotate-[360deg]" />
+              </a>
+            </li>
+            <li 
+              className="list-none" 
+              title="Linkedin">
+              <a 
+                className="relative grid place-items-center rounded-lg transition duration-500 
+                         transform hover:-translate-y-2"
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer external nofollow"
+              >
+                <SocialIcon
+                  url="https://linkedin.com"
+                  className="transition duration-500 transform hover:rotate-[360deg]" />
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="absolute flex gap-8 bottom-[10.125rem] z-[3]">
-          <li className="list-none">
-            <a 
-              className="relative grid place-items-center rounded-lg transition duration-500 
-                         transform hover:-translate-y-2"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SocialIcon
-                url="https://instagram.com"
-                className="transition duration-500 transform hover:rotate-[360deg]" />
-            </a>
-          </li>
-          <li className="list-none">
-            <a 
-              className="relative grid place-items-center rounded-lg transition duration-500 
-                         transform hover:-translate-y-2"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <SocialIcon
-                url="https://linkedin.com"
-                className="transition duration-500 transform hover:rotate-[360deg]" />
-            </a>
-          </li>
-        </ul>
         <div
           className="absolute bottom-0 left-0 z-[4] w-full h-[6.5rem] p-[0.75rem_0.875rem] bg-white text-left 
                     text-[#333] transition duration-500">
