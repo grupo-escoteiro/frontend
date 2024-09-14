a19a8c3679e91d7c08993b8c841b1d37ae5e6210
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-09-14 15:53:05 -0300

    feature(page): add see more in gallery page

---
+++ src/helpers/gallery-images-list.js
@@ -0,0 +1,112 @@
+import image1 from '/gallery/imagem-1.jpg';
+import image2 from '/gallery/imagem-2.jpg';
+import image3 from '/gallery/imagem-3.jpg';
+import image4 from '/gallery/imagem-4.jpg';
+import image5 from '/gallery/imagem-5.jpg';
+import image6 from '/gallery/imagem-6.jpg';
+import image7 from '/gallery/imagem-7.jpg';
+
+const imagesList = [
+  {
+    id: 1,
+    url: image1,
+    alt: ""
+  },
+  {
+    id: 2,
+    url: image2,
+    alt: ""
+  },
+  {
+    id: 3,
+    url: image3,
+    alt: ""
+  },
+  {
+    id: 4,
+    url: image4,
+    alt: ""
+  },
+  {
+    id: 5,
+    url: image5,
+    alt: ""
+  },
+  {
+    id: 6,
+    url: image6,
+    alt: ""
+  },
+  {
+    id: 7,
+    url: image7,
+    alt: ""
+  },
+  {
+    id: 8,
+    url: image1,
+    alt: ""
+  },
+  {
+    id: 9,
+    url: image2,
+    alt: ""
+  },
+  {
+    id: 10,
+    url: image3,
+    alt: ""
+  },
+  {
+    id: 11,
+    url: image4,
+    alt: ""
+  },
+  {
+    id: 12,
+    url: image5,
+    alt: ""
+  },
+  {
+    id: 13,
+    url: image6,
+    alt: ""
+  },
+  {
+    id: 14,
+    url: image7,
+    alt: ""
+  },
+  {
+    id: 15,
+    url: image1,
+    alt: ""
+  },
+  {
+    id: 16,
+    url: image2,
+    alt: ""
+  },
+  {
+    id: 17,
+    url: image3,
+    alt: ""
+  },
+  {
+    id: 18,
+    url: image4,
+    alt: ""
+  },
+  {
+    id: 19,
+    url: image5,
+    alt: ""
+  },
+  {
+    id: 20,
+    url: image6,
+    alt: ""
+  },
+];
+
+export { imagesList };
--- src/pages/public/gallery/index.jsx
+++ src/pages/public/gallery/index.jsx
@@ -1,7 +1,22 @@
-import img1 from '/imagem-1.jpg';
+import { useState } from 'react';
 import { SectionTitle } from '../../../components/section-title';
+import { imagesList } from '../../../helpers/gallery-images-list.js';
+
+const layoutClasses = [
+  'col-span-8 row-span-2',
+  'col-span-4',
+  'col-span-4',
+  'col-span-4',
+  'col-span-8 row-span-2',
+  'col-span-4',
+  'col-span-12'
+];
 
 function Gallery() {
+  const [page, setPage] = useState(1);
+  const partialImagesList = imagesList.filter(image => image.id <= layoutClasses.length * page);
+  const partialImageListHasTheSameSize = partialImagesList.length === imagesList.length;
+
   return (
     <section className="pt-12 pb-20">
       <div className="max-w-[1160px] mx-auto">
@@ -9,66 +24,42 @@ function Gallery() {
           className="pt-12 pb-14"
           content="Galeria"
         />
-        <div className="grid grid-cols-12 gap-8 items-center cursor-pointer">
-          <div className="col-span-8 row-span-2">
-            <img
-              src={img1}
-              alt="Imagem 1"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-4">
-            <img
-              src={img1}
-              alt="Imagem 2"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-4">
-            <img
-              src={img1}
-              alt="Imagem 3"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-4">
-            <img
-              src={img1}
-              alt="Imagem 3"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-8 row-span-2">
-            <img
-              src={img1}
-              alt="Imagem 5"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-4">
-            <img
-              src={img1}
-              alt="Imagem 3"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
-          <div className="col-span-12">
-            <img
-              src={img1}
-              alt="Imagem 6"
-              className="max-w-full max-h-full object-cover shadow-default transition duration-500 hover:scale-105"
-            />
-          </div>
+        <div className="grid grid-cols-12 gap-8 items-center">
+          {partialImagesList.map((image, index) => (
+            <div
+              key={image.id}
+              className={`
+                ${layoutClasses[index % layoutClasses.length]}
+                cursor-pointer w-full h-full
+              `}
+            >
+              <a
+                href={image.url}
+                target="_blank"
+                rel="follow noopener noreferrer"
+                title="Clique para abrir a imagem"
+              >
+                <img
+                  src={image.url}
+                  alt={image.alt}
+                  className="w-full h-full object-cover shadow-default transition duration-500 hover:scale-105"
+                />
+              </a>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-center items-center pt-14">
-          <button
-            className="text-social-gray text-3xl transition duration-500 hover:brightness-75 hover:scale-105"
-            id="loadMore"
-            type="button"
-          >
-            Carregar mais +
-          </button>
-        </div>
+        {!partialImageListHasTheSameSize && (
+          <div className="flex justify-center items-center pt-14">
+            <button
+              className="text-social-gray text-3xl transition duration-500 hover:brightness-75 hover:scale-105"
+              id="loadMore"
+              type="button"
+              onClick={() => setPage(prev => prev + 1)}
+            >
+              Carregar mais +
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
