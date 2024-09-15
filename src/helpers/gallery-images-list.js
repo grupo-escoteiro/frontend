80f4447410e8af8a92e40e1d305674ec50bc6f41
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-09-15 15:37:05 -0300

    Imagens novas

--- src/helpers/gallery-images-list.js
+++ src/helpers/gallery-images-list.js
@@ -5,108 +5,111 @@ import image4 from '/gallery/imagem-4.jpg';
 import image5 from '/gallery/imagem-5.jpg';
 import image6 from '/gallery/imagem-6.jpg';
 import image7 from '/gallery/imagem-7.jpg';
+import image8 from '/gallery/imagem-8.jpg';
+import image9 from '/gallery/imagem-9.jpg';
+import image10 from '/gallery/imagem-10.jpg';
+import image11 from '/gallery/imagem-11.jpg';
+import image12 from '/gallery/imagem-12.jpg';
+import image13 from '/gallery/imagem-13.jpg';
+import image14 from '/gallery/imagem-14.jpg';
+import image15 from '/gallery/imagem-15.jpg';
+import image16 from '/gallery/imagem-16.jpg';
+import image17 from '/gallery/imagem-17.jpg';
+import image18 from '/gallery/imagem-18.jpg';
+import image19 from '/gallery/imagem-19.jpg';
+import image20 from '/gallery/imagem-20.jpg';
+import image21 from '/gallery/imagem-21.jpg';
+import image22 from '/gallery/imagem-22.jpg';
+import image23 from '/gallery/imagem-23.jpg';
+import image24 from '/gallery/imagem-24.jpg';
+import image25 from '/gallery/imagem-25.jpg';
+import image26 from '/gallery/imagem-26.jpg';
+import image27 from '/gallery/imagem-27.jpg';
+import image28 from '/gallery/imagem-28.jpg';
+import image29 from '/gallery/imagem-29.jpg';
+import image30 from '/gallery/imagem-30.jpg';
+import image31 from '/gallery/imagem-31.jpg';
+import image32 from '/gallery/imagem-32.jpg';
+import image33 from '/gallery/imagem-33.jpg';
+import image34 from '/gallery/imagem-34.jpg';
+import image35 from '/gallery/imagem-35.jpg';
+import image36 from '/gallery/imagem-36.jpg';
+import image37 from '/gallery/imagem-37.jpg';
+import image38 from '/gallery/imagem-38.jpg';
+import image39 from '/gallery/imagem-39.jpg';
+import image40 from '/gallery/imagem-40.jpg';
+import image41 from '/gallery/imagem-41.jpg';
+import image42 from '/gallery/imagem-42.jpg';
+import image43 from '/gallery/imagem-43.jpg';
+import image44 from '/gallery/imagem-44.jpg';
+import image45 from '/gallery/imagem-45.jpg';
+import image46 from '/gallery/imagem-46.jpg';
+import image47 from '/gallery/imagem-47.jpg';
+import image48 from '/gallery/imagem-48.jpg';
+import image49 from '/gallery/imagem-49.jpg';
+import image50 from '/gallery/imagem-50.jpg';
+import image51 from '/gallery/imagem-51.jpg';
+import image52 from '/gallery/imagem-52.jpg';
+import image53 from '/gallery/imagem-53.jpg';
+import image54 from '/gallery/imagem-54.jpg';
+import image55 from '/gallery/imagem-55.jpg';
 
 const imagesList = [
-  {
-    id: 1,
-    url: image1,
-    alt: ''
-  },
-  {
-    id: 2,
-    url: image2,
-    alt: ''
-  },
-  {
-    id: 3,
-    url: image3,
-    alt: ''
-  },
-  {
-    id: 4,
-    url: image4,
-    alt: ''
-  },
-  {
-    id: 5,
-    url: image5,
-    alt: ''
-  },
-  {
-    id: 6,
-    url: image6,
-    alt: ''
-  },
-  {
-    id: 7,
-    url: image7,
-    alt: ''
-  },
-  {
-    id: 8,
-    url: image1,
-    alt: ''
-  },
-  {
-    id: 9,
-    url: image2,
-    alt: ''
-  },
-  {
-    id: 10,
-    url: image3,
-    alt: ''
-  },
-  {
-    id: 11,
-    url: image4,
-    alt: ''
-  },
-  {
-    id: 12,
-    url: image5,
-    alt: ''
-  },
-  {
-    id: 13,
-    url: image6,
-    alt: ''
-  },
-  {
-    id: 14,
-    url: image7,
-    alt: ''
-  },
-  {
-    id: 15,
-    url: image1,
-    alt: ''
-  },
-  {
-    id: 16,
-    url: image2,
-    alt: ''
-  },
-  {
-    id: 17,
-    url: image3,
-    alt: ''
-  },
-  {
-    id: 18,
-    url: image4,
-    alt: ''
-  },
-  {
-    id: 19,
-    url: image5,
-    alt: ''
-  },
-  {
-    id: 20,
-    url: image6,
-    alt: ''
-  },
+  { id: 1, url: image1, alt: '' },
+  { id: 2, url: image2, alt: '' },
+  { id: 3, url: image3, alt: '' },
+  { id: 4, url: image4, alt: '' },
+  { id: 5, url: image5, alt: '' },
+  { id: 6, url: image6, alt: '' },
+  { id: 7, url: image7, alt: '' },
+  { id: 8, url: image8, alt: '' },
+  { id: 9, url: image9, alt: '' },
+  { id: 10, url: image10, alt: '' },
+  { id: 11, url: image11, alt: '' },
+  { id: 12, url: image12, alt: '' },
+  { id: 13, url: image13, alt: '' },
+  { id: 14, url: image14, alt: '' },
+  { id: 15, url: image15, alt: '' },
+  { id: 16, url: image16, alt: '' },
+  { id: 17, url: image17, alt: '' },
+  { id: 18, url: image18, alt: '' },
+  { id: 19, url: image19, alt: '' },
+  { id: 20, url: image20, alt: '' },
+  { id: 21, url: image21, alt: '' },
+  { id: 22, url: image22, alt: '' },
+  { id: 23, url: image23, alt: '' },
+  { id: 24, url: image24, alt: '' },
+  { id: 25, url: image25, alt: '' },
+  { id: 26, url: image26, alt: '' },
+  { id: 27, url: image27, alt: '' },
+  { id: 28, url: image28, alt: '' },
+  { id: 29, url: image29, alt: '' },
+  { id: 30, url: image30, alt: '' },
+  { id: 31, url: image31, alt: '' },
+  { id: 32, url: image32, alt: '' },
+  { id: 33, url: image33, alt: '' },
+  { id: 34, url: image34, alt: '' },
+  { id: 35, url: image35, alt: '' },
+  { id: 36, url: image36, alt: '' },
+  { id: 37, url: image37, alt: '' },
+  { id: 38, url: image38, alt: '' },
+  { id: 39, url: image39, alt: '' },
+  { id: 40, url: image40, alt: '' },
+  { id: 41, url: image41, alt: '' },
+  { id: 42, url: image42, alt: '' },
+  { id: 43, url: image43, alt: '' },
+  { id: 44, url: image44, alt: '' },
+  { id: 45, url: image45, alt: '' },
+  { id: 46, url: image46, alt: '' },
+  { id: 47, url: image47, alt: '' },
+  { id: 48, url: image48, alt: '' },
+  { id: 49, url: image49, alt: '' },
+  { id: 50, url: image50, alt: '' },
+  { id: 51, url: image51, alt: '' },
+  { id: 52, url: image52, alt: '' },
+  { id: 53, url: image53, alt: '' },
+  { id: 54, url: image54, alt: '' },
+  { id: 55, url: image55, alt: '' },
 ];
 
 export { imagesList };
