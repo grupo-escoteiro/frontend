eed9d6723c3cfbc14a9c4a0fd6fe3e6de5e114f9
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-09-16 16:13:38 -0300

    Criação da página de configuração

--- src/pages/privates/settings/index.jsx
+++ src/pages/privates/settings/index.jsx
@@ -1,8 +1,60 @@
+import { Container } from '../../../components/container';
 import { Header } from '../../../components/header';
+import { SectionTitle } from '../../../components/section-title';
+import { Tabs } from '../../../components/tabs';
+import { MainPersonalInformations } from './components/main-personal-informations';
 
 function Settings() {
   return (
-    <Header />
+    <>
+      <Header />
+      <section className="pt-12 pb-16">
+        <div className="max-w-[1160px] mx-auto">
+          <SectionTitle 
+            content="Configurações" 
+            className="mb-5"
+          />
+          <Container className="flex py-16 px-8 gap-x-9">
+            <menu>
+              <ul className="flex flex-col gap-6">
+                <li
+                  className="text-nowrap font-semibold text-social-brand
+                              transition duration-500 p-2 hover:bg-social-brand hover:text-social-white
+                              rounded">
+                  <button>
+                    Informações pessoais
+                  </button>                
+                </li>
+                <li
+                  className="font-semibold transition duration-500 p-2 
+                  hover:bg-social-brand hover:text-social-white rounded">
+                  <button>
+                    Segurança
+                  </button>
+                </li>
+                <li
+                  className="font-semibold transition duration-500 p-2 
+                  hover:bg-social-brand hover:text-social-white rounded">
+                  <button>
+                    Conta
+                  </button>
+                </li>
+              </ul>
+            </menu>
+            <div className="border-l border-social-gray w-full">
+              <Tabs 
+                titles={ ['Principal', 'Endereço', 'Imagens'] } 
+                body={
+                  [
+                    <MainPersonalInformations key="1" />
+                  ]
+                }
+              />
+            </div>
+          </Container>
+        </div>
+      </section>
+    </>
   );
 }
 
