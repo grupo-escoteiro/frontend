9dea72f168575828400939742075ab5b3a0f93e5
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-09-29 23:34:27 -0300

    Atualização nas respostas dos faqs

--- src/pages/public/home/index.jsx
+++ src/pages/public/home/index.jsx
@@ -7,27 +7,32 @@ import { Faq } from '../../../components/faq';
 const faqs = [
   {
     question: 'Como posso participar?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: `Basta apenas fazer o cadastro e ir pessoalmente ter uma conversa com os chefes do grupo,
+             mas caso prefira fazer a ficha cadastral pessoalmente, poderá ser feito do mesmo jeito.`,
   },
   {
     question: 'A partir de qual idade posso ser escoteiro?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: `Ramo dos lobinhos poderão participar aos 6 anos de idade até os 10 anos de idade, e depois
+             que completar essa idade, o escoteiro será transferido para o Ramo de júnior.`,
   },
   {
     question: 'Onde os escoteiros se reunem?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: 'A sede do Grupo Escoteiro da Saudade fica localizado na escola Padre Nelson.',
   },
   {
     question: 'Terão responsáveis por perto?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: `Todas as seções tem os chefes como responsáveis pelas crianças e também podem contar com o apoio dos 
+             pais.`,
   },
   {
     question: 'Quais atividades nós fazemos?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: `As atividades são pensadas no desenvolvimento das crianças e dos jovens, ou seja, deixando a criança
+             com a capacidade de se virar sozinha desde cedo.`,
   },
   {
     question: 'Posso acompanhar meus fílhos no escoteiro?',
-    answer: 'Answer the frequently asked question in a simple sentence, a longish paragraph, or even in a list.',
+    answer: `Para acompanhar nas atividades semanais das crianças e dos jovens escoteiros,
+             os pais podem se voluntariar e se tornarem chefes de um dos setores ou de apoio.`,
   },
 ];
 
