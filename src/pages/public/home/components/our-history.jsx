47fd008076d0a23b3ea637cd106f118d09309d26
Matheus G. Pereira <matheusg.pereira89@outlook.com>
2024-10-10 17:55:13 -0300

    feature: add new animation to our history component

--- src/pages/public/home/components/our-history.jsx
+++ src/pages/public/home/components/our-history.jsx
@@ -1,125 +1,171 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { SectionTitle } from '../../../../components/section-title';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 
+const imagesData = [
+  {
+    src: '/gallery/imagem-1.jpg',
+    title: 'Piquenique com as crianças',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-2.jpg',
+    title: 'Eu não sei de nada',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-3.jpg',
+    title: 'Escoteiros em atividade',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-4.jpg',
+    title: 'Culpa do Alemão',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-5.jpg',
+    title: 'Texto Ryan Bolado',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-6.jpg',
+    title: 'Texto teste',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+  {
+    src: '/gallery/imagem-7.jpg',
+    title: 'Texto 4',
+    date: 'xx/mm/yyyy',
+    text: (
+      <p className="pl-16 text-justify">
+        is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
+        has survived not only five centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+        Lorem Ipsum passages, and more recently with desktop publishing
+        software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+    )
+  },
+];
+
 function OurHistory() {
-
-  const imagesData = [
-    {
-      src: '/gallery/imagem-1.jpg',
-      title: 'Piquenique com as crianças',
-      text: (
-        <p className="pl-16 text-justify">
-          is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-          has survived not only five centuries, but also the leap into electronic typesetting, remaining
-          essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing
-          software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      )
-    },
-    {
-      src: '/gallery/imagem-2.jpg',
-      title: 'Eu não sei de nada',
-      text: (
-        <p className="pl-16 text-justify">
-          is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-          has survived not only five centuries, but also the leap into electronic typesetting, remaining
-          essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing
-          software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      )
-    },
-    {
-      src: '/gallery/imagem-3.jpg',
-      title: 'Escoteiros em atividade',
-      text: <p className="pl-16 text-justify">
-        is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-        has survived not only five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing
-        software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    },
-    {
-      src: '/gallery/imagem-4.jpg',
-      title: 'Culpa do Alemão',
-      text: <p className="pl-16 text-justify">
-        is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-        has survived not only five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing
-        software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    },
-    {
-      src: '/gallery/imagem-5.jpg',
-      title: 'Texto Ryan Bolado',
-      text: <p className="pl-16 text-justify">
-        is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-        has survived not only five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing
-        software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    },
-    {
-      src: '/gallery/imagem-6.jpg',
-      title: 'Texto teste',
-      text: <p className="pl-16 text-justify">
-        is simply dummy text of the printing and typesetting industry.
-        Lorem Ipsum has been the standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-        has survived not only five centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing
-        software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    },
-    {
-      src: '/gallery/imagem-7.jpg',
-      title: 'Texto 4',
-      text: (
-        <p className="pl-16 text-justify">
-          is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book. It
-          has survived not only five centuries, but also the leap into electronic typesetting, remaining
-          essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing
-          software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-      )
-    },
-  ];
-
+  const imageRefs = useRef([]);
   const [selectedImage, setSelectedImage] = useState(imagesData[0]);
 
-  function handleImageClick(imageData) {
+  function handleImageClick(imageData, e) {
     setSelectedImage(imageData);
+    addOutlineOnSelectedImage(e);
   };
 
+  function removeOutlineOnDeselectedImage() {
+    imageRefs.current.forEach(image => {
+      if (image) {
+        image.classList.remove(
+          'outline',
+          'outline-social-brand',
+          'outline-[2px]',
+          'scale-105'
+        );
+      }
+    });
+  }
+
+  function addOutlineOnSelectedImage(e) {
+    removeOutlineOnDeselectedImage();
+    e.target.classList.add(
+      'outline',
+      'outline-social-brand',
+      'outline-[2px]',
+      'scale-105'
+    );
+  }
+
   function generateImagesBoard() {
-    return imagesData.map((imageData, mainImage) => (
+    return imagesData.map((imageData, index) => (
       <img
-        key={mainImage}
-        className="w-24 h-24 rounded-full cursor-pointer object-cover hover:scale-105 transition duration-500"
-        title={imageData.title}
+        key={`${index}-${imageData.text}-${imageData.title}-${imageData.src}`}
+        className={`
+          w-24 h-24 rounded-full cursor-pointer object-cover hover:scale-105
+          transition duration-500
+          ${index === 0 && 'outline outline-social-brand outline-[2px] scale-105'}
+        `}
+        title={imageData.date}
+        aria-describedby={imageData.text}
         src={imageData.src}
-        alt={`Imagem ${mainImage + 1}`}
-        onClick={() => handleImageClick(imageData)}
+        alt={`Imagem ${index + 1}`}
+        onClick={e => handleImageClick(imageData, e)}
+        draggable={false}
+        ref={el => (imageRefs.current[index] = el)}
       />
     ));
   }
@@ -140,9 +186,11 @@ function OurHistory() {
               exit={{ opacity: 0 }}
               transition={{ duration: 0.5 }}
               className="w-[580px] h-[420px] rounded-md cursor-pointer object-cover shadow-lg"
-              title={selectedImage.title}
+              title={selectedImage.date}
               src={selectedImage.src}
               alt={selectedImage.title}
+              aria-describedby={selectedImage.date}
+              draggable={false}
             />
           </AnimatePresence>
           <div className="text-center w-full">
