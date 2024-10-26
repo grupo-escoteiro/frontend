133d0520f626915f1c4c36ff2cd3b33e98d28045
SayRyan <richardryancarvalho.infotarde@gmail.com>
2024-10-26 15:17:44 -0300

    Criação dos 4 steps

--- src/pages/public/auth/register/components/third-step.jsx
+++ src/pages/public/auth/register/components/third-step.jsx
@@ -1,17 +1,18 @@
-import { Button } from '../../../../../components/button';
 import { Container } from '../../../../../components/container';
 import { PageTransition } from '../../../../../components/page-transition';
 import { Step } from '../../../../../components/step';
 import { Link } from 'react-router-dom';
+import { Button } from '../../../../../components/button';
 import boneco from '/bonecopipa.svg';
 
-function SecondStep() {
+function ThirdStep() {
   return (
     <PageTransition>
       <Container className="grid grid-cols-[1fr_3fr] gap-x-7 w-[1160px]">
         <div
-          className="text-social-white h-full w-full bg-no-repeat
-                      bg-cover bg-[url('/register-flow/box-side-background.png')]">
+          className="text-social-white w-full h-full bg-no-repeat 
+                       bg-cover bg-[url('/register-flow/box-side-background.png')]"
+        >
           <div
             className={`
               w-full h-full justify-evenly flex-col py-16 px-8 flex items-center
@@ -20,90 +21,106 @@ function SecondStep() {
             <h1 className="mb-16 text-xl text-social-white font-semibold">
               Faça seu cadastro
             </h1>
-            <img src={boneco} />
+            <img src={boneco} alt="Ilustração" />
           </div>
         </div>
         <div className="py-16 px-8 gap-y-8 flex flex-col items-center">
           <Step 
             steps={4}
-            active={2}
+            active={3}
           />
           <form className="w-full max-w-3xl">
             <fieldset className="grid grid-cols-2 gap-x-6 gap-y-6 pb-8">
               <div className="flex flex-col gap-y-2">
                 <label 
-                  htmlFor="name"
+                  htmlFor="cep"
                   className="font-semibold"
                 >
-                    Nome completo
+                    CEP
                 </label>
                 <input 
                   type="text"
-                  name="nome"
-                  id="name"
-                  placeholder="Digite seu nome completo"
-                  className="w-full h-16 px-5 py-5 rounded border border-social-gray"
+                  name="cep"
+                  id="cep"
+                  placeholder="xxxxx-xxx"
+                  className="w-full h-16 px-4 py-2 rounded border border-social-gray"
                 />
               </div>
               <div className="flex flex-col gap-y-2">
                 <label 
-                  htmlFor="cpf"
+                  htmlFor="rua"
                   className="font-semibold"
                 >
-                CPF
+                Rua
                 </label>
                 <input 
                   type="text"
-                  name="cpf"
-                  id="cpf"
-                  placeholder="Digite seu cpf"
-                  className="w-full h-16 px-5 py-5 rounded border border-social-gray"
+                  name="rua"
+                  id="rua"
+                  placeholder="Digite sua rua"
+                  className="w-full h-16 px-4 py-2 rounded border border-social-gray"
+                />
+              </div>
+            </fieldset>
+            <fieldset className="grid grid-cols-3 gap-x-6 gap-y-6 pb-8">
+              <div className="flex flex-col gap-y-2">
+                <label 
+                  htmlFor="bairro"
+                  className="font-semibold"
+                >
+                  Bairro
+                </label>
+                <input 
+                  type="text"
+                  name="bairro"
+                  id="bairro"
+                  placeholder="Digite seu bairro"
+                  className="w-full h-16 px-4 py-2 rounded border border-social-gray"
                 />
               </div>
               <div className="flex flex-col gap-y-2">
                 <label 
-                  htmlFor="data"
+                  htmlFor="cidade"
                   className="font-semibold"
                 >
-                    Data de nascimento
+                  Cidade
                 </label>
                 <input 
-                  type="date"
-                  name="data"
-                  id="data"
-                  className="w-full h-16 px-5 py-5 rounded border border-social-gray"
+                  type="text"
+                  name="cidade"
+                  id="cidade"
+                  placeholder="Digite sua cidade"
+                  className="w-full h-16 px-4 py-2 rounded border border-social-gray"
                 />
               </div>
               <div className="flex flex-col gap-y-2">
                 <label 
-                  htmlFor="telefone"
+                  htmlFor="estado"
                   className="font-semibold"
                 >
-                    Telefone
+                  Estado
                 </label>
-                <input 
-                  type="tel"
-                  name="telefone"
-                  id="telefone"
-                  placeholder="(xx) xxxxx-xxxx"
-                  className="w-full h-16 px-5 py-5 rounded border border-social-gray"
-                />
+                <select id="estado" className="w-full h-16 px-4 py-2 rounded border border-social-gray">
+                  <option>SP</option>
+                  <option>RJ</option>
+                  <option>AM</option>
+                </select>
               </div>
-              <div className="flex flex-col gap-y-2">
-                <label 
-                  htmlFor="telefone-emer"
-                  className="font-semibold"
-                >
-                    Telefone de emergência
-                </label>
-                <input 
-                  type="tel"
-                  name="telefone-emergencia"
-                  id="telefone-emer"
-                  placeholder="(xx) xxxxx-xxxx"
-                  className="w-full h-16 px-5 py-5 rounded border border-social-gray"
-                />
-              </div>
+            </fieldset>
+            <fieldset className="flex flex-col gap-y-2 pb-8">
+              <label 
+                htmlFor="complemento"
+                className="font-semibold"
+              >
+                    Complemento
+              </label>
+              <input 
+                type="text"
+                name="complemento"
+                id="complemento"
+                placeholder="Escreva seu complemento"
+                className="w-full h-16 px-4 py-2 rounded border border-social-gray"
+              />
             </fieldset>
             <fieldset className="flex justify-end items-center gap-x-6 mt-5">
               <Link
@@ -126,4 +143,4 @@ function SecondStep() {
   );
 }
 
-export { SecondStep };
+export { ThirdStep };
