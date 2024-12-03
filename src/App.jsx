eb01af016939b3e13cf0e1a40daad45f678436a2
luisfesantos <luis.santos585@etec.sp.gov.br>
2024-12-02 21:34:27 -0300

    Att

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,12 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { DefaultRouters } from './routers';
 import { AccessibilityContext } from './contexts/accessibility-context';
 import { AuthContextProvider } from './contexts/auth-context';
 import { BrowserRouter } from 'react-router-dom';
 import { useOnlineStatus } from './hooks/useOnline';
-import { toast } from 'sonner';
 
 function App() {
   const { speak } = useContext(AccessibilityContext);
-  const [weather, setWeather] = useState([]);
-
-  useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      fetch(
-        // eslint-disable-next-line max-len
-        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=e24bc461ba064bbcc009e2ea74f9e864&lang=pt_br
-      `).then(r => r.json())
-        .then(r => {
-          const { weather: weatherApi } = r;
-          setWeather(weatherApi);
-        });
-    },
-    (error) => console.error('Erro ao obter localização', error.message));
-
-  }, [setWeather]);
-
-  useEffect(() => {
-    toast.warning('Previsão do Tempo' ,{
-      description: weather[0]?.description,
-    });
-  });
 
   useOnlineStatus();
 
