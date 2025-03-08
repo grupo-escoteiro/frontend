0f14712ecb4688fb79ac5ab09a2b80e576e9c822
SayRyan <richardryancarvalho.infotarde@gmail.com>
2025-03-08 09:49:55 -0300

    fire

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyDd7dkLNZS-4r2jtXWQfWzTNIRAJvOmnmA',
   authDomain: 'inclusao-social-ec3b0.firebaseapp.com',
   projectId: 'inclusao-social-ec3b0',
-  storageBucket: 'inclusao-social-ec3b0.appspot.com',
+  storageBucket: 'inclusao-social-ec3b0.firebasestorage.app',
   messagingSenderId: '699586735512',
   appId: '1:699586735512:web:89f266fdf35afc3df70fed',
   measurementId: 'G-3SLRHWZSL7'
